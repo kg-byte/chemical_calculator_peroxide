16887b00301ef40dba55a5578d7860d8921692e0
Kim G <xiaole.guo@yahoo.com>
2022-07-27 18:14:07 -0600

    create chemical object with properties retrieved from cirpy

---
+++ src/chemical.py
@@ -0,0 +1,9 @@
+import cirpy 
+
+class Chemical:
+	def __init__(self, name, cas, max_con, product_name):
+		self.name = name 
+		self.mw = cirpy.resolve(cas, 'MW')
+		self.smile = cirpy.resolve(cas, 'smiles') 
+		self.max_con = max_con 
+		self.product_name = product_name 
---
+++ test/test_chemical.py
@@ -0,0 +1,13 @@
+import pytest
+import sys
+sys.path.insert(1, '/Users/xiaoleguo/kg_byte/fun/chemical_calculator/src')
+import chemical 
+
+class TestChemical():
+	def test_chemical_properties(self):
+		sample = chemical.Chemical('water', '7732-18-5', 35, 'product_1')
+		assert sample.name == 'water'
+		assert sample.mw == '18.0152'
+		assert sample.smile == 'O'
+		assert sample.product_name == 'product_1'
+		
